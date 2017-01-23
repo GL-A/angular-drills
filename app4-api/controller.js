988c4eb756d574804047fb120373ad338c40ecd7
George Lopez-Ayala <georgelopezayala@icloud.com>
2017-01-23 10:03:49 -0700

    dd

--- app4-api/controller.js
+++ app4-api/controller.js
@@ -1,10 +1,17 @@
 angular.module('apiApp').controller('ctrl', function($scope, service){
   $scope.test = service.test;
 
-  $scope.starWars = function(){
-    service.starWars().then(function(response){
-        //console.log(response);
-      })
-  }
-  $scope.starWars();
+  // $scope.starWars = function(){
+  //   service.starWars().then(function(response){
+  //       //console.log(response);
+  //       return response;
+  //     })
+  // }
+  // $scope.starWars();
+
+
+  service.starWars().then(function(response){
+    $scope.name = response.data;
+    $scope.work = response.data.abilities;
+  })
 })
--- app4-api/service.js
+++ app4-api/service.js
@@ -5,7 +5,7 @@ angular.module('apiApp').service('service', function($http){
   this.starWars = function(){
     return $http({
       method: 'GET',
-      url: 'http://pokeapi.co/api/v2/pokemon/'
+      url: 'http://pokeapi.co/api/v2/pokemon/1/'
     }).then(function(response){
       console.log(response);
       return response;
